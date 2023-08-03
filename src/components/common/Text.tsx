2e0c1c1e90979a815ed8d45922795c2eab438888
Nicolas Maillols <maillols@winch.fr>
2023-08-03 20:38:00 +0200

    change

--- src/components/common/Text.tsx
+++ src/components/common/Text.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 import { StyleSheet, TextProps } from 'react-native';
 import configManager from '~/manager/configManager';
 
-const fontFamily = configManager.isIos() ? "sfPro" : "montserrat";
+const fontFamily = configManager.isIos() ? 'sfPro' : 'montserrat';
 
 export function Regular(props: TextProps) {
-    return <Text {...props} style={[props.style, { fontFamily: fontFamily + "-regular" }]} />;
+    return <Text {...props} style={[props.style, { fontFamily: fontFamily + '-regular' }]} />;
 }
 export function Bold(props: TextProps) {
-    return <Text {...props} style={[props.style, { fontFamily: fontFamily + "-bold" }]} />;
+    return <Text {...props} style={[props.style, { fontFamily: fontFamily + '-bold' }]} />;
 }
 export function Thin(props: TextProps) {
-    return <Text {...props} style={[props.style, { fontFamily: fontFamily + "-thin" }]} />;
+    return <Text {...props} style={[props.style, { fontFamily: fontFamily + '-thin' }]} />;
 }
 export function Italic(props: TextProps) {
-    return <Text {...props} style={[props.style, { fontFamily: fontFamily + "-italic" }]} />;
+    return <Text {...props} style={[props.style, { fontFamily: fontFamily + '-italic' }]} />;
 }
 export default function Title(props: TextProps) {
     return <Bold {...props} style={[styles.title, props.style]} />;
